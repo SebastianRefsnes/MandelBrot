0e71961df7c64660f0bfe4f0c9787c7053b6d104
SebastianRefsnes <sebastian.refsnes@gmail.com>
2020-03-30 15:16:45 +0200

    Some coloring improvements, added smoothing, program does not really use it yet

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 window.onload = init;
 let canvas,context,imageHeight,imageWidth,maxIters,xMininum,yMinimum,scaleFactor;
+
+const colorMap = [
+  "66,30,15",
+  "25,7,26",
+  "9,1,47",
+  "4,4,73",
+  "0,7,100",
+  "12,44,138",
+  "24,82,177",
+  "57,125,209",
+  "134,181,229",
+  "211, 236, 248",
+  "241, 233, 191",
+  "248, 201, 95",
+  "255, 170, 0",
+  "204, 128, 0",
+  "153, 87, 0",
+  "106, 52, 3"
+];
+
 function init(){
   canvas = document.getElementById("MandelCanv");
   canvas.addEventListener("mousedown",mouseClickHandler)
@@ -9,11 +29,11 @@ function init(){
   imageWidth = canvas.width;
 
   xCent = -0.5; //-0.5
-  yCent = -0; //0
+  yCent = 0; //0
 
-  scaleFactor = 200;
+  scaleFactor = 400;
 
-  maxIters = 500;
+  maxIters = 200;
   draw();
 }
 
@@ -46,17 +66,20 @@ function draw(){
       let cY = xyToComplex(x,y)[1];
 
       let iterations = findIterations(cX, cY, maxIters);
+      let num = Math.sin(iterations);
       //coloring c:
-      let hue = scale(iterations,0,maxIters,360,0);
+      let hue = scale(num,-1,1,360,0);
       let sat = '100%';
       let light = '40%';
 
-      let r = iterations%Math.round(maxIters/2);
-      let g = iterations%Math.round(maxIters/2);
-      let b = iterations%Math.round(maxIters/2);
+      let rgb = colorMap[Math.floor(scale(num,-1,1,0,15))];
+      if(iterations >= maxIters || iterations <= 0){
+        rgb = "0,0,0"
+        light = "0%";
+      }
 
       context.fillStyle = `hsl(${hue},${sat},${light})`;
-      //context.fillStyle = `rgb(${r},${g},${b})`
+      context.fillStyle = `rgb(${rgb})`
       context.fillRect(x,y,1,1);
     }
   }
@@ -65,13 +88,17 @@ function draw(){
 function findIterations(centX,centY,maxIters){
   let i = 0;
   let zR = 0, zI = 0;
-  while(i < maxIters && Math.pow(zR,2) + Math.pow(zI,2) < Math.pow(2,2)){
+  while(i < maxIters && Math.pow(zR,2) + Math.pow(zI,2) < 4){
     tempVal = Math.pow(zR,2) - Math.pow(zI,2) + centX;
     zI = 2 * zR * zI + centY;
     zR = tempVal;
     i++;
   }
-  return i;
+  if(i == maxIters){
+    return i;
+  }else{
+    return (i + 1) - Math.log2(Math.log2(Math.sqrt(Math.pow(zR,2)+Math.pow(zI,2))) / Math.log(2));
+  };
 }
 
 //Maps a given number from its range to some other range
